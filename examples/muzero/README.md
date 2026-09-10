8497f6521cef69267f04381522e36ed7377c4338
David Gray <davidgraymi@gmail.com>
2026-09-10 15:00:36 -0500

    make a new example that fully implements chess self play

---
+++ examples/muzero/README.md
@@ -0,0 +1,32 @@
+# MuZero example
+
+A simple (Gumbel) AlphaZero [[Silver+18](https://www.science.org/doi/10.1126/science.aar6404), [Danihelka+22](https://openreview.net/forum?id=bERaNdoegnO)] example using [Mctx](https://github.com/deepmind/mctx) library. See [Pgx paper](https://openreview.net/forum?id=UvX8QfhfUx) for more details.
+
+![](assets/pgx-az-training.png)
+
+> [!NOTE]
+> This implementation of AlphaZero demonstrates sufficient learning performance in environments including 9x9 Go, but it has some slight differences in learning details compared to the original AlphaZero and Gumbel AlphaZero. An implementation that addresses these differences and focuses on enhanced efficiency is currently under development and is expected to be released shortly.
+
+## Usage
+
+Note that you need to install `jax` and `jaxlib` in addition to the packages written in `requirements.txt` according to your execution environment.
+
+```sh
+$ pip install -U pip && pip install -r requirements.txt
+$ python3 train.py env_id=go_9x9 seed=0
+```
+
+## Reference
+
+- [[Silver+18](https://www.science.org/doi/10.1126/science.aar6404)] "A general reinforcement learning algorithm that masters
+chess, shogi, and go through self-play"
+- [[Danihelka+22](https://openreview.net/forum?id=bERaNdoegnO)] "Policy improvement by planning with Gumbel"
+
+
+## Change history
+
+- **[#1107](https://github.com/sotetsuk/pgx/pull/1107)** Extract `compute_loss_input` ([wandb report](https://api.wandb.ai/links/sotetsuk/979hmps8)).
+- **[#1106](https://github.com/sotetsuk/pgx/pull/1106)** Use `optax.softmax_cross_entropy` ([wandb report](https://api.wandb.ai/links/sotetsuk/8w0or84k)).
+- **[#1088](https://github.com/sotetsuk/pgx/pull/1088)** Adjust to API v2 ([wandb report](https://api.wandb.ai/links/sotetsuk/0g44pjsg)).
+- **[#1055](https://github.com/sotetsuk/pgx/pull/1055)** Use default Gumbel AlphaZero hyperparameters ([wandb report](https://api.wandb.ai/links/sotetsuk/o8752t54)).
+- **[#1026](https://github.com/sotetsuk/pgx/pull/1026)** Initial version. Supposed to reproduce the [Pgx paper](https://openreview.net/forum?id=UvX8QfhfUx) results ([wandb report](https://api.wandb.ai/links/sotetsuk/5q30e5n9)).
---
+++ examples/muzero/train.py
@@ -0,0 +1,409 @@
+# Copyright 2023 The Pgx Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import datetime
+import os
+import pickle
+import time
+from functools import partial
+from typing import NamedTuple
+from pydantic import ConfigDict
+import shutil
+
+import haiku as hk
+import jax
+import jax.numpy as jnp
+import mctx
+import optax
+import pgx
+import wandb
+from omegaconf import OmegaConf
+from pgx.experimental import auto_reset
+from pydantic import BaseModel
+
+from network import AZNet
+
+devices = jax.local_devices()
+num_devices = len(devices)
+
+
+class Config(BaseModel):
+    env_id: pgx.EnvId = "chess"
+    seed: int = 0
+    max_num_iters: int = 400
+    # network params
+    num_channels: int = 128
+    num_layers: int = 6
+    resnet_v2: bool = True
+    # selfplay params
+    selfplay_batch_size: int = 1024
+    num_simulations: int = 32
+    max_num_steps: int = 256
+    # training params
+    training_batch_size: int = 4096
+    learning_rate: float = 0.001
+    # eval params
+    eval_interval: int = 5
+    model_config = ConfigDict(extra="forbid")
+
+
+conf_dict = OmegaConf.from_cli()
+config: Config = Config(**conf_dict)
+print(config)
+
+env = pgx.make(config.env_id)
+
+
+def forward_fn(x, is_eval=False):
+    net = AZNet(
+        num_actions=env.num_actions,
+        num_channels=config.num_channels,
+        num_blocks=config.num_layers,
+        resnet_v2=config.resnet_v2,
+    )
+    policy_out, value_out = net(x, is_training=not is_eval, test_local_stats=False)
+    return policy_out, value_out
+
+
+forward = hk.without_apply_rng(hk.transform_with_state(forward_fn))
+optimizer = optax.adam(learning_rate=config.learning_rate)
+
+
+def recurrent_fn(model, rng_key: jnp.ndarray, action: jnp.ndarray, state: pgx.State):
+    # model: params
+    # state: embedding
+    del rng_key
+    model_params, model_state = model
+
+    current_player = state.current_player
+    state = jax.vmap(env.step)(state, action)
+
+    (logits, value), _ = forward.apply(model_params, model_state, state.observation, is_eval=True)
+    # mask invalid actions
+    logits = logits - jnp.max(logits, axis=-1, keepdims=True)
+    logits = jnp.where(state.legal_action_mask, logits, jnp.finfo(logits.dtype).min)
+
+    reward = state.rewards[jnp.arange(state.rewards.shape[0]), current_player]
+    value = jnp.where(state.terminated, 0.0, value)
+    discount = -1.0 * jnp.ones_like(value)
+    discount = jnp.where(state.terminated, 0.0, discount)
+
+    recurrent_fn_output = mctx.RecurrentFnOutput(
+        reward=reward,
+        discount=discount,
+        prior_logits=logits,
+        value=value,
+    )
+    return recurrent_fn_output, state
+
+
+class SelfplayOutput(NamedTuple):
+    obs: jnp.ndarray
+    reward: jnp.ndarray
+    terminated: jnp.ndarray
+    action_weights: jnp.ndarray
+    discount: jnp.ndarray
+
+
+@jax.pmap
+def selfplay(model, rng_key: jnp.ndarray) -> SelfplayOutput:
+    model_params, model_state = model
+    batch_size = config.selfplay_batch_size // num_devices
+
+    def step_fn(state, key) -> SelfplayOutput:
+        key1, key2 = jax.random.split(key)
+        observation = state.observation
+
+        (logits, value), _ = forward.apply(
+            model_params, model_state, state.observation, is_eval=True
+        )
+        root = mctx.RootFnOutput(prior_logits=logits, value=value, embedding=state)
+
+        policy_output = mctx.gumbel_muzero_policy(
+            params=model,
+            rng_key=key1,
+            root=root,
+            recurrent_fn=recurrent_fn,
+            num_simulations=config.num_simulations,
+            invalid_actions=~state.legal_action_mask,
+            qtransform=mctx.qtransform_completed_by_mix_value,
+            gumbel_scale=1.0,
+        )
+        actor = state.current_player
+        keys = jax.random.split(key2, batch_size)
+        state = jax.vmap(auto_reset(env.step, env.init))(state, policy_output.action, keys)
+        discount = -1.0 * jnp.ones_like(value)
+        discount = jnp.where(state.terminated, 0.0, discount)
+        return state, SelfplayOutput(
+            obs=observation,
+            action_weights=policy_output.action_weights,
+            reward=state.rewards[jnp.arange(state.rewards.shape[0]), actor],
+            terminated=state.terminated,
+            discount=discount,
+        )
+
+    # Run selfplay for max_num_steps by batch
+    rng_key, sub_key = jax.random.split(rng_key)
+    keys = jax.random.split(sub_key, batch_size)
+    state = jax.vmap(env.init)(keys)
+    key_seq = jax.random.split(rng_key, config.max_num_steps)
+    _, data = jax.lax.scan(step_fn, state, key_seq)
+
+    return data
+
+
+class Sample(NamedTuple):
+    obs: jnp.ndarray
+    policy_tgt: jnp.ndarray
+    value_tgt: jnp.ndarray
+    mask: jnp.ndarray
+
+
+@jax.pmap
+def compute_loss_input(data: SelfplayOutput) -> Sample:
+    batch_size = config.selfplay_batch_size // num_devices
+    value_mask = jnp.cumsum(data.terminated[::-1, :], axis=0)[::-1, :] >= 1
+
+    def body_fn(carry, i):
+        ix = config.max_num_steps - i - 1
+        v = data.reward[ix] + data.discount[ix] * carry
+        return v, v
+
+    _, value_tgt = jax.lax.scan(
+        body_fn,
+        jnp.zeros(batch_size),
+        jnp.arange(config.max_num_steps),
+    )
+    value_tgt = value_tgt[::-1, :]
+
+    return Sample(
+        obs=data.obs,
+        policy_tgt=data.action_weights,
+        value_tgt=value_tgt,
+        mask=value_mask,
+    )
+
+
+def loss_fn(model_params, model_state, samples: Sample):
+    (logits, value), model_state = forward.apply(
+        model_params, model_state, samples.obs, is_eval=False
+    )
+
+    policy_loss = optax.softmax_cross_entropy(logits, samples.policy_tgt)
+    policy_loss = jnp.mean(policy_loss)
+
+    value_loss = optax.l2_loss(value, samples.value_tgt)
+    value_loss = jnp.mean(value_loss * samples.mask)
+
+    return policy_loss + value_loss, (model_state, policy_loss, value_loss)
+
+
+@partial(jax.pmap, axis_name="i")
+def train(model, opt_state, data: Sample):
+    model_params, model_state = model
+    grads, (model_state, policy_loss, value_loss) = jax.grad(loss_fn, has_aux=True)(
+        model_params, model_state, data
+    )
+    grads = jax.lax.pmean(grads, axis_name="i")
+    updates, opt_state = optimizer.update(grads, opt_state)
+    model_params = optax.apply_updates(model_params, updates)
+    model = (model_params, model_state)
+    return model, opt_state, policy_loss, value_loss
+
+
+@jax.pmap
+def evaluate(rng_key, my_model, baseline_model):
+    """Evaluates the live learning model against a stable baseline snapshot model."""
+    my_player = 0
+    my_model_params, my_model_state = my_model
+    base_model_params, base_model_state = baseline_model
+
+    key, subkey = jax.random.split(rng_key)
+    batch_size = config.selfplay_batch_size // num_devices
+    keys = jax.random.split(subkey, batch_size)
+    state = jax.vmap(env.init)(keys)
+
+    def body_fn(val):
+        key, state, R = val
+        # Policy output for the active learning agent
+        (my_logits, _), _ = forward.apply(
+            my_model_params, my_model_state, state.observation, is_eval=True
+        )
+        # Policy output for the snapshot target baseline agent
+        (opp_logits, _), _ = forward.apply(
+            base_model_params, base_model_state, state.observation, is_eval=True
+        )
+        
+        is_my_turn = (state.current_player == my_player).reshape((-1, 1))
+        logits = jnp.where(is_my_turn, my_logits, opp_logits)
+        
+        # Mask out illegal moves during evaluation to keep games valid
+        logits = logits - jnp.max(logits, axis=-1, keepdims=True)
+        logits = jnp.where(state.legal_action_mask, logits, jnp.finfo(logits.dtype).min)
+        
+        key, subkey = jax.random.split(key)
+        action = jax.random.categorical(subkey, logits, axis=-1)
+        state = jax.vmap(env.step)(state, action)
+        R = R + state.rewards[jnp.arange(batch_size), my_player]
+        return (key, state, R)
+
+    _, _, R = jax.lax.while_loop(
+        lambda x: ~(x[1].terminated.all()), body_fn, (key, state, jnp.zeros(batch_size))
+    )
+    return R
+
+
+if __name__ == "__main__":
+    wandb.init(project="pgx-chess-muzero", config=config.model_dump())
+
+    # Prepare checkpoint dir
+    now = datetime.datetime.now(datetime.timezone(datetime.timedelta(hours=9)))
+    now = now.strftime("%Y%m%d%H%M%S")
+    ckpt_dir = os.path.join("checkpoints", f"{config.env_id}_{now}")
+    os.makedirs(ckpt_dir, exist_ok=True)
+    
+    rng_key = jax.random.PRNGKey(config.seed)
+    rng_key, subkey = jax.random.split(rng_key)
+    
+    # Initialize networks
+    init_state = jax.vmap(env.init)(jax.random.split(subkey, num_devices))
+    model_params, model_state = forward.init(
+        jax.random.split(subkey, num_devices), init_state.observation, is_eval=False
+    )
+    
+    # --- OPTIONAL: Load Supervised Learning weights if they exist ---
+    # TODO: optionally load the latest weights
+    sl_weights = os.path.join("checkpoints", "sl_weights.pkl")
+    if os.path.exists(sl_weights):
+        print("Found supervised checkpoint! Loading weights...")
+        with open(sl_weights, "rb") as f:
+            model_params, model_state = pickle.load(f)
+
+    model = (model_params, model_state)
+    opt_state = optimizer.init(model_params)
+    model, opt_state = jax.device_put_replicated((model, opt_state), devices)
+    
+    # Replicate model variables across local accelerator devices
+    model = jax.device_put_replicated(model, devices)
+    opt_state = jax.device_put_replicated(opt_state, devices)
+    
+    # Establish our starting baseline evaluator snapshot from our current network weights
+    baseline_model = jax.tree_util.tree_map(lambda x: jnp.copy(x), model)
+
+    # --- OPTIONAL: Load baseline weights if they exist ---
+    # TODO: optionally load the latest weights
+    base_weights = os.path.join("checkpoints", "base_weights.pkl")
+    if os.path.exists(base_weights):
+        print("Found baseline checkpoint! Loading weights...")
+        with open(base_weights, "rb") as f:
+            baseline_model = pickle.load(f)
+
+    # Initialize logging dict
+    iteration: int = 0
+    hours: float = 0.0
+    frames: int = 0
+    metrics = {"iteration": 0, "hours": hours, "frames": frames}
+
+    while True:
+        # Evaluation
+        if iteration % config.eval_interval == 0:
+            rng_key, eval_key = jax.random.split(rng_key)
+            keys = jax.random.split(eval_key, num_devices)
+            R = evaluate(keys, model, baseline_model)
+
+            win_rate = ((R == 1).sum() / R.size).item()
+            draw_rate = ((R == 0).sum() / R.size).item()
+            loss_rate = ((R == -1).sum() / R.size).item()
+
+            metrics.update(
+                {
+                    f"eval/vs_baseline/avg_R": R.mean().item(),
+                    f"eval/vs_baseline/win_rate": win_rate,
+                    f"eval/vs_baseline/draw_rate": draw_rate,
+                    f"eval/vs_baseline/lose_rate": loss_rate,
+                }
+            )
+
+            # Store checkpoints
+            model_0, opt_state_0 = jax.tree_util.tree_map(lambda x: x[0], (model, opt_state))
+            chpt_0 = os.path.join(ckpt_dir, f"{iteration:06d}.ckpt")
+            with open(chpt_0, "wb") as f:
+                dic = {
+                    "config": config,
+                    "rng_key": rng_key,
+                    "model": jax.device_get(model_0),
+                    "opt_state": jax.device_get(opt_state_0),
+                    "iteration": iteration,
+                    "frames": frames,
+                    "hours": hours,
+                    "pgx.__version__": pgx.__version__,
+                    "env_id": env.id,
+                    "env_version": env.version,
+                }
+                pickle.dump(dic, f)
+
+            # Upgrades the baseline when the network shows meaningful improvement
+            if win_rate > 0.55:
+                print(">>> Model outperformed old baseline snapshot. Updating target model parameters.")
+                baseline_model = jax.tree_util.tree_map(lambda x: jnp.copy(x), model)
+                shutil.copy2(chpt_0, base_weights)
+
+        print(metrics)
+        wandb.log(metrics)
+
+        if iteration >= config.max_num_iters:
+            break
+
+        iteration += 1
+        metrics = {"iteration": iteration}
+        st = time.time()
+
+        # Selfplay
+        rng_key, subkey = jax.random.split(rng_key)
+        keys = jax.random.split(subkey, num_devices)
+        data: SelfplayOutput = selfplay(model, keys)
+        samples: Sample = compute_loss_input(data)
+
+        # Shuffle samples and make minibatches
+        samples = jax.device_get(samples)  # (#devices, batch, max_num_steps, ...)
+        frames += samples.obs.shape[0] * samples.obs.shape[1] * samples.obs.shape[2]
+        samples = jax.tree_util.tree_map(lambda x: x.reshape((-1, *x.shape[3:])), samples)
+        rng_key, subkey = jax.random.split(rng_key)
+        ixs = jax.random.permutation(subkey, jnp.arange(samples.obs.shape[0]))
+        samples = jax.tree_util.tree_map(lambda x: x[ixs], samples)  # shuffle
+        num_updates = samples.obs.shape[0] // config.training_batch_size
+        minibatches = jax.tree_util.tree_map(
+            lambda x: x.reshape((num_updates, num_devices, -1) + x.shape[1:]), samples
+        )
+
+        # Training
+        policy_losses, value_losses = [], []
+        for i in range(num_updates):
+            minibatch: Sample = jax.tree_util.tree_map(lambda x: x[i], minibatches)
+            model, opt_state, policy_loss, value_loss = train(model, opt_state, minibatch)
+            policy_losses.append(policy_loss.mean().item())
+            value_losses.append(value_loss.mean().item())
+        policy_loss = sum(policy_losses) / len(policy_losses)
+        value_loss = sum(value_losses) / len(value_losses)
+
+        et = time.time()
+        hours += (et - st) / 3600
+        metrics.update(
+            {
+                "train/policy_loss": policy_loss,
+                "train/value_loss": value_loss,
+                "hours": hours,
+                "frames": frames,
+            }
+        )
